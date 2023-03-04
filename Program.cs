4a347cf68b083a9eea3de178070b705c85d8930c
natashandrigoz <nata_shandrigoz@mail.ru>
2023-03-04 19:08:45 +0200

    Удаление из списка

--- Program.cs
+++ Program.cs
@@ -28,23 +28,46 @@ namespace ConsoleApp10
         static void Main(string[] args)
         {
             //Создание пустого списка
-            List<string> people = new List<string>();
+            //List<string> people = new List<string>();
             //Добавление в список
             //1.Добавление нового элемента в список
-            people.Add("Bob");
-            Output(people);//вызов доп. метода для вывода коллекции
-            // people = { "Bob" };
+            //people.Add("Bob");
+            //Output(people);//вызов доп. метода для вывода коллекции
+            //people = { "Bob" };
             //2.Добавление в список массива
-            people.AddRange(new[] { "Tom","Sam", "Alice" });
-            Output(people);
+            //people.AddRange(new[] { "Tom","Sam", "Alice" });
+            //Output(people);
             // people = { "Bob", "Tom", "Sam", "Alice" };
             //3. Вставка элемента item в список по индексу index
-            people.Insert(0,"Eugene");
-            Output(people);
+            //people.Insert(0,"Eugene");
+            //Output(people);
+            //people = { "Eugene", "Tom", "Bob", "Sam", "Alice" };
             //4. Вставка коллекции элементов collection в текущий список начиная с индекса index
             //people.InsertRange(1, new[] { "Mike", "Kate" });
-            people.InsertRange(1, new string [] { "Mike", "Kate" });
+            //people.InsertRange(1, new string [] { "Mike", "Kate" });
+            //Output(people);
+            // people = { "Eugene", "Mike", "Kate", "Tom", "Bob", "Sam", "Alice" };
+            
+            //Удаление из списка
+            //Создание списка
+            var people = new List<string> { "Eugene", "Mike", "Kate", "Tom", "Bob", "Sam", "Tom", "Alice" };
+            //1. Удаление элемента по указанному индексу index (второй элемент)
+            people.RemoveAt(1);
             Output(people);
+            // people = { "Eugene", "Kate", "Tom", "Bob", "Sam", "Tom", "Alice" };
+            //2. Удаление элемента item ("Tom") из списка
+            people.Remove("Tom");
+            Output(people);
+            // people = { "Eugene", "Kate", "Bob", "Sam", "Tom", "Alice" };
+            //3. Удаление из списка 2 элемента начиная с индекса 1
+            people.RemoveRange(1, 2);
+            Output(people);
+            //people = { "Eugene", "Sam", "Tom", "Alice" };
+            //4. Удаление из списка всех элементов
+            people.Clear();
+            //people = {  };
+
+
             Console.Read();
             
         }
